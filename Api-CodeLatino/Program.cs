3fac1963dfdb875be9702bdda0670f42ef1f2f84
ByJelias05 <garciassierra@hotmail.com>
2025-06-16 20:58:26 -0400

    Configuro CORS para permitir peticiones desde cualquier origen

--- Api-CodeLatino/Program.cs
+++ Api-CodeLatino/Program.cs
@@ -17,6 +17,14 @@ builder.Services.AddSwaggerGen();
 // Configuro el DbContext para usar SQL Server con la cadena de conexion extraida
 builder.Services.AddDbContext<CodeLatinoContext>(options => options.UseSqlServer(conncetionString));
 
+// Configuro CORS para permitir peticiones desde cualquier origen
+builder.Services.AddCors(options =>
+    options.AddPolicy("AllowWeb", policy =>
+       policy.WithOrigins("https://codelatino.onrender.com")
+       .AllowAnyHeader()
+       .AllowAnyMethod()
+    ));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +33,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowWeb");
 
 app.UseHttpsRedirection();
 
