cd0bf2d2f66d00346844bbaac34e7c9ce955f90f
ByJelias05 <garciassierra@hotmail.com>
2025-06-16 20:53:38 -0400

    Configurando el program con DbContext pra la conexion la Base de datos

--- Api-CodeLatino/Program.cs
+++ Api-CodeLatino/Program.cs
@@ -1,4 +1,11 @@
+using Api_CodeLatino.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+//Extraigo la cadena de conexion desde el archivo de configuracion
+var conncetionString = builder.Configuration.GetConnectionString("");
 
 // Add services to the container.
 
@@ -6,6 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configuro el DbContext para usar SQL Server con la cadena de conexion extraida
+builder.Services.AddDbContext<CodeLatinoContext>(options => options.UseSqlServer(conncetionString));
 
 var app = builder.Build();
 
